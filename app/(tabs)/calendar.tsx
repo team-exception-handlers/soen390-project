73ad95f6db1882cdcd799c37c03a9935539f0811
Priyesh Patel <priyeshpatel1216@gmail.com>
2026-02-23 20:19:50 -0500

    Fixed comment

--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -118,7 +118,7 @@ export default function CalendarScreen() {
         }
     }, [response]);
 
-    // Fetch events
+    // Fetch events that are stored in the Calender API
     useEffect(() => {
         if (accessToken) {
             loadEvents(accessToken, false);
