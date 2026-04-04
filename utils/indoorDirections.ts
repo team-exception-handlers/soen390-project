1f601692b505c487a945bff37175e7ca70659f4e
Samuel Richard <40695080+samuelrichard122@users.noreply.github.com>
2026-04-04 00:16:44 -0400

    Add tests for findIndoorRouteToNodeId and orthogonalizeSegmentPoints functions

--- utils/indoorDirections.ts
+++ utils/indoorDirections.ts
@@ -1545,6 +1545,7 @@ export function getGraphFloorBounds(
 
 export const __indoorDirectionsTestUtils = {
   getFloorQuery,
+  orthogonalizeSegmentPoints,
   buildGraph,
   dijkstra,
   formatFloorLabel,
